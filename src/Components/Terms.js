5787678ce9baac9b4941769c47caa4cc57874fa6
Nimisha Goel <144768630+codekiosk@users.noreply.github.com>
2023-10-10 16:15:01 +0530

    Revert "Revert "Revert "Revert "ngo app""""
    This reverts commit 13b53548fab54d98c2b757a1ec685033d716bc7a.

---
+++ src/Components/Terms.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import Header from '../Components/ReusableComponents/Header'
+import Footer from './ReusableComponents/Footer'
+import Grid from '@mui/material/Grid';
+
+const Terms = () => {
+  return (
+    <div>
+        <Header/>
+        <div style={{margin:"50px"}}>
+            <h1 className='teamtext'>TERMS AND CONDITIONS</h1>
+        </div>
+        <Grid container spacing={2} className='choice1'>
+            <Grid item xs={8} className='termsgrid'>
+                <div style={{marginBottom:"70px"}}>
+                <h1>Terms of Use</h1>
+                <h2>1. Introduction</h2>
+                <p>
+                These are the Terms and Conditions of Use for the SankalpTaru Foundation online website.
+                <br/>
+                <br/>
+                SankalpTaru Foundation is an NGO, having its office at Plot no. 12, Dhoran Khas, Kishanpur, Near ST. Xaviers School, Canal Road, Uttarakhand -248001, India which has its online portal at https://sankalptaru.org/ (the “Site”). By using (browsing or donating on) our Site or by subscribing to or using any of our services (“Services”), you agree to comply with and be bound by the following terms of use (collectively, the “Terms of Use”).
+                <br/><br/>
+                Please review the following terms carefully. If you do not agree to any of these terms, please do not use this Site. These Terms of Use and various other policies are binding as per the provisions of the Information Technology (Intermediaries Guidelines) Rules 2011 formulated under the Information Technology Act 2000.
+                <br/><br/>
+                The Term ‘ST’ or ‘us’ or ‘we’ refers to the owner of the website, i.e., SankalpTaru Foundation. Our NGO registration number is 368/211 – 2012 and is registered in Dehradun, Uttarakhand, India. The term “you” or “your” refers to you, the user or viewer of our site or any of the Services.
+                <br/><br/>
+                The use of this website is subject to the following terms of use:
+                </p>
+                </div>
+
+                <div style={{marginBottom:"70px"}}>
+                <h2>2. Changes to the terms of Use</h2>
+                <p>
+                SankalpTaru Foundation reserves the right to change any of the terms and conditions contained in these Terms of Use or any policies or guidelines governing the Site or Services at any time and in its sole discretion. Changes to the Terms of Use and referenced policies and guidelines may be posted without notice to you. Your continued use of our Site and Services following any changes will constitute your acceptance of such changes or modifications.
+                <br/><br/>
+                We are committed to ensuring that your information is secure. In order to prevent unauthorized access or disclosure, we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.
+                <br/><br/>
+                Neither we, nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.
+                <br/><br/>
+                Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through this website meet your specific requirements.
+                <br/><br/>
+                From time to time this website may also include links to other websites. These links are provided for your convenience to provide further information. They do not signify that we endorse the website(s). We have no responsibility for the content of the linked website(s). Use of any such linked web site is at the user’s own risk.
+                <br/><br/>
+                You may not create a link to this website from another website or document without ST’s prior written consent.
+                </p>
+                </div>
+
+                <div style={{marginBottom:"70px"}}>
+                <h2>3. SankalpTaru Services Description</h2>
+                <p>
+                SankalpTaru Foundation is an NGO registered under The Societies Registration Act, 1860. It aims to create greenery on Earth with the contribution from corporate organizations, their employees, individuals, environmental agencies and funding entities. ST gives its users an opportunity to select a site from listed options to plant trees. We use internet and associated information technology enabled processes to ensure transparency and visibility. We issue a certificate as a token of appreciation to planters and also post the same on social networks if the user opts for the same.
+                <br/><br/>
+                In lieu of our services following points should be noted:
+                <br/><br/>
+                ST runs the tree plantation and rural livelihood program across India. We follow a strict due diligence process to select sites for plantation and Program Beneficiaries.
+                <br/><br/>
+                ST and its affiliates adopts best practices to plant trees and make best efforts to maintain trees post plantation. However, ST shall not be responsible for any harm to planted trees which may occur due to reasons beyond our control (fire, flood, earthquake, drought, diseases and land acquisition by government authorities). We ensure planted trees are intact and safe, however ST shall not be responsible for the harm occurring from illegal felling or grazing of trees.
+                <br/><br/>
+                Sometimes it may be possible that we may tag already planted tree to your virtual tree. This is done only under exceptional circumstances. We maintain a separate lot to meet the aforementioned eventuality. However, ST NEVER tags trees which have already been tagged to other planters.
+                <br/><br/>
+                Wherever contributors/planters are interested to visit plantation sites they are encouraged to do so on their own expenses.
+                <br/><br/>
+                Planters do not own trees or any products derived from the trees planted by them.
+                <br/><br/>
+                We as an NGO shall be under no liability whatsoever in respect of any loss or damage arising directly or indirectly out of the decline of authorization for any Transaction, on Account of the Cardholder having exceeded the preset limit mutually agreed by us with our acquiring bank from time to time.
+                </p>
+                </div>
+
+                <div style={{marginBottom:"70px"}}>
+                <h2>4.     User License & Site Access</h2>
+                <p>
+                Subject to the terms and conditions of these Terms of Use, ST hereby grants you a limited, revocable, non-transferable and non-exclusive license to access and use the Site by displaying it on your internet browser only for the purpose of browsing content, searching for trees, and uploading reviews. Users may not use the Site for any commercial use or use on behalf of any third party.
+                <br/><br/>
+                Permission is granted to temporarily download one copy of the materials (information or software) on ST’s website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not reproduce, distribute, display, sell, lease, transmit, create derivative works from, translate, modify, reverse-engineer, disassemble, decompile or otherwise exploit this Site or any portion of it by any means, unless expressly permitted by ST. For the removal of doubt, it is clarified that unlimited or wholesale reproduction, copying of the content for commercial or non-commercial purposes and unwarranted modification of data and information within the content of the Site is not permitted.
+                <br/><br/>
+                You may not use the Site or any materials obtained from the Site to develop, or as a component of, any information, storage and retrieval system, database, information base, or similar resource (in any media now existing or hereafter developed), that is offered for commercial distribution of any kind, including through sale, lease, license, rental, subscription, or any other commercial distribution mechanism. You also may not “mirror” or “frame” any material contained on the Site on any other server.
+                <br/><br/>
+                You may not use any manual or automated software, devices, or other processes to “crawl” or “spider” any web pages contained in the Site. You may not monitor or copy, or allow others to monitor or copy, our web pages or the content included herein. Furthermore, you agree not to take any action that interferes with the proper working of or places an unreasonable load on our infrastructure, including but not limited to unsolicited communications, attempts to gain unauthorized access, or transmission or activation of computer viruses.
+                <br/><br/>
+                You shall not upload to, distribute, or otherwise publish on our Site any content, information, or other material that (a) violates or infringes the copyrights, patents, trademarks, service marks, trade secrets, or other proprietary rights of any person or organization, unless you are the owner of such rights or have permission from their rightful owner to post the material and to grant ST all of the license rights granted herein; (b) is libelous, threatening, defamatory, obscene, indecent, pornographic, harassing, hateful, racially or ethnically offensive, or encourages conduct that would be considered a criminal offense, or could give rise to any civil or criminal liability under India or international law; (c) includes any bugs, viruses, worms, trap doors, Trojan horses or other harmful code or properties, (d) advertises or solicits business, or (e) impersonates another person.
+                <br/><br/>
+                Any breach of these Terms of Use may result in the immediate revocation of the license granted above, without notice to you.
+                </p>
+                </div>
+            </Grid>
+        </Grid>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Terms
